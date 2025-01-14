b4f40a35746f777656c010b202328d0d951ced74
Nikesh Pathak <nikspathak91@gmail.com>
2025-01-13 21:28:02 -0500

    Added another module geometry

---
+++ mymath/geometry.py
@@ -0,0 +1,14 @@
+def area_of_rectangle(length, breadth):
+    '''
+    This function takes length and breadth as input and 
+    return the area of the rectangle
+    '''
+    area = length * breadth
+    return area
+def area_of_circle(radius):
+    '''
+    This function takes radius as a input and
+    return the area of the circle
+    '''
+    area = (22/7) * radius * radius
+    return area
